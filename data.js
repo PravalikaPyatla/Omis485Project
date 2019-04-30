a301ed79e55a50eb347d95f0019e978099b6ca97
PravalikaPyatla <46685120+PravalikaPyatla@users.noreply.github.com>
2019-04-30 17:27:08 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+var data = [
+{name:"Anurag Gudipati",course:"Javascript",contact:"5867011210",address:"753 Fotis Drive, Dekalb",price:"40",rating:"4",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"Sravya Gattamaneni",course:"Javascript",contact:"8158975690",address:"940 Regent Drive, Dekalb",price:"50",rating:"5",datefrom:"04/28/2019-",dateto:"04/30/2019-"},
+{name:"Samantha Akkineni",course:"Javascript",contact:"9875678765",address:"795 Regent Drive, Dekalb",price:"55",rating:"4",datefrom:"04/03/2019-",dateto:"04/12/2019-"}, 
+{name:"RamCharan Konidela",course:"Javascript",contact:"6486752907",address:"1056 Spiros Court, Dekalb",price:"35",rating:"3",datefrom:"04/20/2019-",dateto:"04/28/2019-"},
+{name:"Vamsidhar Kella",course:"Python",contact:"8159098167",address:"867 Fotis Drive, Dekalb",price:"45",rating:"4",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"KalyanRam Nandamuri",course:"Python",contact:"8674902765",address:"345 WestRidge, Dekalb",price:"60",rating:"5",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"Venu Madhav Maddala",course:"Python",contact:"6809875642",address:"567 Regent Drive, Dekalb",price:"40",rating:"4",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"Bhavana Ramarapu",course:"Python",contact:"5497862390",address:"689 Fotis Drive, Dekalb",price:"55",rating:"4",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"Viswanath Kotha",course:"JQuery",contact:"7864569087",address:"546 Fotis Drive, Dekalb",price:"45",rating:"4",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"Nikhil Yarlagadda",course:"JQuery",contact:"8762347890",address:"879 WestRidge, Dekalb",price:"60",rating:"5",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"Sandeep Kohli",course:"JQuery",contact:"3427685678",address:"908 Regent Drive, Dekalb",price:"40",rating:"4",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"Aayush Sharma",course:"JQuery",contact:"9076547235",address:"125 Fotis Drive, Dekalb",price:"55",rating:"4",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"Deepika Padukone",course:"HTML",contact:"9874563987",address:"675 Fotis Drive, Dekalb",price:"45",rating:"4",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"Ranbir Kapoor",course:"HTML",contact:"8156789876",address:"987 WestRidge, Dekalb",price:"60",rating:"5",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"Alia Bhatt",course:"HTML",contact:"5896547865",address:"684 Regent Drive, Dekalb",price:"40",rating:"4",datefrom:"04/24/2019-",dateto:"04/30/2019-"},
+{name:"Ranveer Singh",course:"HTML",contact:"6308763456",address:"792 Fotis Drive, Dekalb",price:"55",rating:"4",datefrom:"04/24/2019-",dateto:"04/30/2019-"},	];
